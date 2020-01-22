c20479556729949a90b810986ad050a14f25109a
tkwiek <kwiek.t@gmail.com>
2020-01-22 16:30:43 +0100

    Add Dal

---
+++ LunchManager/LunchManager.Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchManager.Models
+{
+    [Table("User", Schema = "LunchManager")]
+
+    public class User
+    {
+        [Key]
+        [Column("User_Id")]
+        public int Id { get; set; }
+
+
+
+    }
+}
